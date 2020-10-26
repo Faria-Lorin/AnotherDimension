c2bebe2dd667b5168e2f5e35cf22d85135ec1c5e
Faria-Lorin <florin20@stuy.edu>
2020-10-25 22:08:53 -0400

    I wrote a code from sumRows and tested out an array with 5 rows (I didn't give the array any specific values, so all values were 0) and I tested it out in the temporary main method and the otput was a sting of 5 0's, which is what I expected

--- ArrayOps.java
+++ ArrayOps.java
@@ -16,11 +16,27 @@ public class ArrayOps{
     return max;
   }
 
+  public static int[] sumRows(int[][] matrix) {
+      int[] arr = new int[matrix.length];
+      int[] sums = new int[matrix.length];
+      for (int row = 0; row < matrix.length; row++){
+      for (int col = 0; col < matrix[0].length; col++){
+        sums[col] = matrix[row][col];
+        ArrayOps.sum(sums);
+      }
+      arr[row] = sums[row];
+    }
+      return arr;
+  }
+
+
   public static void main(String[] args) {
   /*int [] x = {1,2,3,4,5,5};
-    System.out.println(ArrayOps.sum(x));*/
+    System.out.println(ArrayOps.sum(x));
     int [] x = {1,2,3,4,15,5};
-    System.out.println(ArrayOps.largest(x));
+    System.out.println(ArrayOps.largest(x));*/
+    int[][] matrix = new int [5][2];
+    System.out.println(Arrays.toString(sumRows(matrix)));
   }
 
 

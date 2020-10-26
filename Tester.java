74e76ffe4d8674255288bc382c8dca529d0ec532
Faria-Lorin <florin20@stuy.edu>
2020-10-26 02:25:50 -0400

    I took the main method I had in ArrayOps.java and put it in Tester.java and added some code to print labels for the outputs to make the outputs of each method look more organized when they are printed and be easier to read

---
+++ Tester.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+public class Tester{
+
+    public static void main(String[] args) {
+      System.out.println("\nTests for sum of an array:");
+      int [] x = {1,2,3,4,5,5};
+      System.out.println(ArrayOps.sum(x));
+
+      System.out.println("\nTests for largest element of an array:");
+      int [] q = {1,2,3,4,15,5};
+      System.out.println(ArrayOps.largest(q));
+
+      int[][] arr = new int [5][2];
+      for (int row = 0; row < arr.length; row++){
+      for (int col = 0; col < arr[0].length; col++){
+        arr[row][col] = 2;
+      }
+    }
+
+
+          int i = 10;
+          int[][] matrix = new int [5][2];
+          for (int row = 0; row < matrix.length; row++){
+          for (int col = 0; col < matrix[0].length; col++){
+            matrix[row][col] = i;
+            i++;
+
+          }
+        }
+      System.out.println("\nTests for sum of rows:");
+      System.out.println(Arrays.toString(ArrayOps.sumRows(matrix)));
+      System.out.println("\nTests for sum of columns:");
+      System.out.println(Arrays.toString(ArrayOps.sumCols(matrix)));
+      System.out.println("\nTests for largest in rows:");
+      System.out.println(Arrays.toString(ArrayOps.largestInRows(matrix)));
+      System.out.println("\nTests for sum of all values in an array:");
+      System.out.println(ArrayOps.sum(matrix));
+      System.out.println("\nTests for isRowMagic:");
+      System.out.println(ArrayOps.isRowMagic(matrix));
+      System.out.println("\nTests for isColMagic:");
+      System.out.println(ArrayOps.isColMagic(matrix));
+    }
+}
